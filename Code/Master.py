17235ee312702542dc9fdb5111386d7f272bf6b4
Andy Boomer <aboomer07@gmail.com>
2021-01-30 00:14:19 -0800

    Adding some initial analysis

---
+++ Code/Master.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+from sklearn.covariance import GraphicalLasso, GraphicalLassoCV
+import os
+import sys
+import re
+import matplotlib.pyplot as plt
+
+currDir = os.getcwd()
+
+df = pd.read_csv(os.path.abspath("..") + "/Data/finance.csv", header=None)
+with open(os.path.abspath("..") + "/Data/DataIndex.csv", 'r') as f:
+	df_index = f.read()
+
+df_index = df_index.replace("\xa0", "").replace(" = ", ",")
+split = re.search("(?<=\\n)..+(?=column 0)", df_index).span()[0]
+rows = df_index[:(split - 1)].replace("row ", "")
+cols = df_index[split:].replace("column ", "")
+
+rows = pd.DataFrame([row.split(",") for row in rows.split("\n")])
+rows.columns = ['Date', 'Row']
+
+cols = pd.DataFrame([col.split(",") for col in cols.split("\n")])
+cols.columns = ['Ticker', 'Column']
+
+df.columns = cols['Ticker'].values
+df.index = rows['Date'].values
+
+emp_cov = np.matmul(df.values.T, df.values)
+
+cov = GraphicalLasso(max_iter=100, alpha=2, tol=0.1, enet_tol=0.1).fit(df)
+covCV = GraphicalLassoCV().fit(df.values)
+
+G = nx.from_numpy_matrix(cov.covariance_)
+G1 = nx.node_connected_component(G)
