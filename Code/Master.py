c7de97bea90bd69e443f49fe0549e48b02a393cf
Andy Boomer <aboomer07@gmail.com>
2021-02-07 12:27:13 -0800

    Merging

--- Code/Master.py
+++ Code/Master.py
@@ -29,10 +29,10 @@ df.index = rows['Date'].values
 
 emp_cov = np.matmul(df.values.T, df.values)
 
-cov = GraphicalLasso(max_iter=300, alpha=1.25, tol=0.01, enet_tol=0.01).fit(df)
+cov = GraphicalLasso(max_iter=300, alpha=1.5, tol=0.01, enet_tol=0.01).fit(df)
 covCV = GraphicalLassoCV().fit(df.values)
 
-G = nx.from_numpy_matrix(cov.covariance_)
+G = nx.from_numpy_matrix(cov.get_precision())
 
 nodes = list(G.nodes(data=True))
 names_dict = {val[0]: cols['Ticker'].values[i] for i, val in enumerate(nodes)}
@@ -41,7 +41,7 @@ G = nx.relabel_nodes(G, names_dict)
 graphs = [G.subgraph(c).copy() for c in nx.connected_components(G)]
 graphs = [g for g in graphs if len(g) > 1]
 
-amzn = G.subgraph([list(names_dict.values()).index('AMZN')])
+amzn = G.subgraph([list(names_dict.values()).index('GOOG')])
 
 nx.draw_networkx(graphs[0], pos=nx.spring_layout(graphs[0]),
 	node_size=[len(v) * 200 for v in graphs[0].nodes()])
