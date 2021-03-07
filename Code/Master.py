b05683a324ffe7ebfcc151d0221bb04cd93107c3
Andy Boomer <aboomer07@gmail.com>
2021-03-07 10:05:56 +0100

    Updated Code

--- Code/Master.py
+++ Code/Master.py
@@ -80,7 +80,18 @@ cols.columns = ['Ticker', 'Column']
 df.columns = cols['Ticker'].values
 df.index = rows['Date'].values
 
-df = df.sample(50, axis=1)
+dates = ['2010-01-13', '2010-01-19', '2010-01-22', '2010-01-27', '2010-02-01',
+    '2010-02-04', '2010-02-09', '2010-02-12', '2010-02-18', '2010-02-23',
+    '2010-02-26', '2010-03-03', '2010-03-08', '2010-03-11', '2010-03-16',
+    '2010-03-19']
+
+stock_cols = [2, 321, 30, 241, 48, 180]
+
+stock_names = ['Apple', 'MSFT', 'AMAZON', 'Intel', 'Boeing', 'Fedex']
+
+time_set = [[93, 95], [96, 98], [99, 101], [102, 104], [105, 107], [108, 110], [111, 113], [114, 116], [117, 119], [120, 122], [123, 125], [126, 128], [129, 131], [132, 134], [135, 137], [138, 140]]
+
+df = df.iloc[time_set[0][0]:time_set[-1][1], stock_cols]
 
 ################################################################################
 # Section 3
@@ -96,13 +107,14 @@ covCV = GraphicalLassoCV().fit(df.values)
 G = nx.from_numpy_matrix(cov.get_precision())
 
 cov = GraphicalLasso(max_iter=300, alpha=0.01, tol=0.01, enet_tol=0.01, verbose=True).fit(df)
-covCV = GraphicalLassoCV().fit(df.values)
+covCV = GraphicalLassoCV(alphas=list(np.linspace(0, 1.5, 50)), max_iter=300, tol=0.01, enet_tol=0.01, verbose=True).fit(df)
 
 #### VISUALIZATION ###
 
-p = cov.precision_  # get precision matrix
+p = covCV.precision_  # get precision matrix
 
-sns.heatmap(p)  # get a quick look
+sns.heatmap(p, annot=True, xticklabels=stock_names, yticklabels=stock_names)
+plt.set_title('Delta Precision Matrix ' + dates[i])
 plt.show()
 
 #### ALPHA RANGE ###
@@ -149,47 +161,45 @@ for alpha in alphas:
 # Section 4
 ################################################################################
 
-from TVGL import *
+empcov = np.load(os.getcwd() + "/Data/empcov.npy")
+cov = np.load(os.getcwd() + "/Data/cov.npy")
+theta_diff = np.load(os.getcwd() + "/Data/theta_diff.npy")
+theta_est = np.load(os.getcwd() + "/Data/theta_est.npy")
 
-lamb = 2.5
-beta = 12
-lengthOfSlice = 10
+if not os.path.isdir(outputDir + "/empcov"):
+    os.mkdir(outputDir + "/empcov")
+empcovDir = outputDir + "/empcov/"
 
-thetaSet = TVGL(df, lengthOfSlice, lamb, beta, indexOfPenalty = 3, verbose=True)
+if not os.path.isdir(outputDir + "/cov"):
+    os.mkdir(outputDir + "/cov")
+covDir = outputDir + "/cov/"
 
-#### VISUALIZATION ANDY ###
+if not os.path.isdir(outputDir + "/theta_diff"):
+    os.mkdir(outputDir + "/theta_diff")
+covDir = outputDir + "/theta_diff/"
 
-# G = nx.from_numpy_matrix(cov.covariance_)
+if not os.path.isdir(outputDir + "/theta_est"):
+    os.mkdir(outputDir + "/theta_est")
+covDir = outputDir + "/theta_est/"
 
-# nodes = list(G.nodes(data=True))
-# names_dict = {val[0]: cols['Ticker'].values[i] for i, val in enumerate(nodes)}
-# G = nx.relabel_nodes(G, names_dict)
+G = nx.from_numpy_matrix(np.matrix(theta_est[0]), create_using=nx.Graph)
+layout = nx.circular_layout(G)
+nx.draw(G, layout, ax=ax[1])
+nx.draw_networkx_labels(G, pos=layout, labels=dict(zip(range(6), stock_names)))
 
-# graphs = [G.subgraph(c).copy() for c in nx.connected_components(G)]
-# graphs = [g for g in graphs if len(g) > 1]
+for i in range(len(dates)):
 
-# amzn = G.subgraph([list(names_dict.values()).index('GOOG')])
+    fig, ax = plt.subplots(2, 1, sharex=True, sharey=True)
 
-# nx.draw_networkx(graphs[0], pos=nx.spring_layout(graphs[0]),
-#                  node_size=[len(v) * 200 for v in graphs[0].nodes()])
-# plt.show()
+    sns.heatmap(theta_est[i], annot=True, xticklabels=stock_names, yticklabels=stock_names, ax=ax[0])
+    ax[0].set_title('Precision Matrix: ' + dates[i])
+    sns.heatmap(theta_diff[i], annot=True, xticklabels=stock_names, yticklabels=stock_names, ax=ax[1], vmin=min([theta_diff[i].min() for i in range(theta_diff.shape[0])]), vmax=max([theta_diff[i].max() for i in range(theta_diff.shape[0])]))
+    ax[1].set_title('Delta Precision Matrix ' + dates[i])
 
-# nx.draw_networkx(graphs[0], pos=nx.spring_layout(graphs[0]),
-#                  node_size=[len(v) * 200 for v in graphs[0].nodes()])
-# plt.show()
+    plt.savefig(outputDir + "/PrecMats" + str(i).zfill(3) + ".png")
+    plt.close()
 
-# prepare the matrix for network illustration
-# p = pd.DataFrame(p, columns=df.columns, index=df.columns)
-# links = p.stack().reset_index()
-# links.columns = ['var1', 'var2', 'value']
-# links = links.loc[(abs(links['value']) > 0.17) & (links['var1'] != links['var2'])]
-
-# # build the graph using networkx lib
-# G = nx.from_pandas_edgelist(links, 'var1', 'var2', create_using=nx.Graph())
-# spring_pos = nx.spring_layout(G, k=0.2 * 1 / np.sqrt(len(G.nodes())), iterations=20)
-# circle_pos = nx.circular_layout(G)
-# plt.figure(3, figsize=(15, 15))
-# nx.draw(G, pos=circle_pos)
-# nx.draw_networkx_labels(G, pos=circle_pos)
-# plt.savefig(outputDir + 'NetworkGraph.png')
-# plt.close()
+os.chdir("Output")
+os.system(
+    "ffmpeg -framerate 1 -i PrecMats%3d.png -c:v h264 -r 30 -s 1920x1080 ./Heatmaps.mp4")
+os.chdir("..")
